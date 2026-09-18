1c133d7f1885a1c57a95ab0efd9776f9a0b43534
spartacusrex <patrickcerri@gmail.com>
2026-09-18 09:22:24 +0100

    1.3.3

--- app/src/main/java/org/minimarex/minimacore/service/MinimaService.java
+++ app/src/main/java/org/minimarex/minimacore/service/MinimaService.java
@@ -259,7 +259,7 @@ public class MinimaService extends Service {
 
         //TESTER HACK
         //vars.add("-clean");
-        vars.add("-solo");
+        //vars.add("-solo");
 
         vars.add("-nosyncibd");
 
